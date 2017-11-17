d14983d25596e823def5eee718a2e6a81f3db85e
kristina06 <33395991+kristina06@users.noreply.github.com>
2017-11-17 21:49:55 +0300

    решила первую задачу
    http://informatics.mccme.ru/mod/statements/view3.php?id=276&chapterid=2959#1

---
+++ study-vs/kr/lab2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+	int x;
+	scanf("%d", &x);
+	if (x > 0) {
+		x = 1;
+	}
+	else if (x < 0) {
+		x = -1;
+	}
+	else {
+		x = 0;
+	}
+	printf("%d", x);
+	return 0;
+}
